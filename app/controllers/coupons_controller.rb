6c36e9d26ea9cb24c26b31b2f6b4f3fc6b8fff50
Robert Wayne <robertjwayne@gmail.com>
2017-05-19 15:58:25 -0400

    Add create and index actions

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,2 +1,13 @@
 class CouponsController < ApplicationController
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+  def index
+    @coupons = Coupon.all
+  end
 end
